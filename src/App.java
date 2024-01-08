b3eb66c332651cb718f64770569b8f51eb37b8ef
Minenko-V-S <minenkovitalij@gmail.com>
2024-01-08 21:43:21 +0500

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        //instentiate the GUI
+        PasswordGeneratorGUI passwordGeneratorGUI = new PasswordGeneratorGUI();
+
+        //make the GUI visible
+        passwordGeneratorGUI.setVisible(true);
+    }
+}
